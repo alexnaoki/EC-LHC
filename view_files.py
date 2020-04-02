b79a6541a483d7df9d6793028557c7aefe162838
alexnaoki <alexkobayashi10@gmail.com>
2020-04-01 22:11:24 -0300

    Add new tab

--- view_files.py
+++ view_files.py
@@ -62,7 +62,8 @@ class Viewer:
         self.button04 = ipywidgets.Button(description='Plot')
         self.button04.on_click(self.button_plot_lfdl)
 
-        self.x_scale02 = bq.LinearScale()
+        # self.x_scale02 = bq.LinearScale()
+        self.x_scale02 = bq.DateScale()
         self.y_scale02 = bq.LinearScale()
         self.xax02 = bq.Axis(scale=self.x_scale02, label='x')
         self.yax02 = bq.Axis(scale=self.y_scale02, label='y', orientation='vertical')
@@ -75,15 +76,50 @@ class Viewer:
         self.out02 = ipywidgets.Output(layout={'border': '1px solid black'})
 
         self.tab4 = ipywidgets.VBox([ipywidgets.HBox([self.column_x_axis_lfdl, self.column_y_axis_lfdl]), self.button04, self.fig02,self.out02])
-
         ####################
 
+        ####    TAB5    ####
+        self.path_01 = ipywidgets.Text(placeholder='Insert CSV files from EC')
+        self.path_02 = ipywidgets.Text(placeholder='Insert LowFreq data')
 
-        self.tabs = ipywidgets.Tab(children=[self.tab1, self.tab2, self.tab3, self.tab4])
+        self.button05 = ipywidgets.Button(description='Show All')
+        self.button05.on_click(self._button_show_all)
+
+        self.list_files01 = []
+        self.list_files02 = []
+        self.select01 = ipywidgets.SelectMultiple(options=self.list_files01,description='CSVs files',layout={'width': 'max-content'})
+        self.select02 = ipywidgets.SelectMultiple(options=self.list_files02,description='LowFreq files',layout={'width': 'max-content'})
+        self.select01.observe(self._create_lines03, 'value')
+        self.select02.observe(self._create_lines04, 'value')
+        # self.select02.observe(self._create_lines03)
+
+        self.column_names_01 = []
+        self.column_y_01 = ipywidgets.Dropdown(options=self.column_names_01, description='Y-Axis EC')
+        self.column_names_02 = []
+        self.column_y_02 = ipywidgets.Dropdown(options=self.column_names_02, description='Y-Axis LowFreq')
+
+        self.button06 = ipywidgets.Button(description='Plot Compare')
+        self.button06.on_click(self._button_plot_compare)
+
+        self.x_scale03 = bq.DateScale()
+        self.y_scale03 = bq.LinearScale()
+        self.xax03 = bq.Axis(scale=self.x_scale03, label='x')
+        self.yax03_01 = bq.Axis(scale=self.y_scale03, label='y1', orientation='vertical', side='left')
+        self.yax03_02 = bq.Axis(scale=self.y_scale03, label='y2', orientation='vertical', side='right')
+        self.fig03 = bq.Figure(marks=[], axes=[self.xax03, self.yax03_01, self.yax03_02], animation_duration=1000)
+
+
+        self.out03 = ipywidgets.Output(layout={'border':'1px solid black'})
+
+        self.tab5 = ipywidgets.VBox([ipywidgets.HBox([self.path_01, self.path_02]),self.button05,ipywidgets.VBox([self.select01, self.select02]),ipywidgets.HBox([self.column_y_01, self.column_y_02]), self.button06, self.fig03, self.out03])
+        ####################
+
+        self.tabs = ipywidgets.Tab(children=[self.tab1, self.tab2, self.tab3, self.tab4, self.tab5])
         self.tabs.set_title(0, 'Input Data Eddy Covariance')
         self.tabs.set_title(1, 'Plot EddyCovariance data')
         self.tabs.set_title(2, 'Input Data Datalogger LF')
         self.tabs.set_title(3, 'Plot Low Frequency Datalogger')
+        self.tabs.set_title(4, 'Compare')
         display(self.tabs)
 
     def _create_lines(self, *args):
@@ -93,6 +129,23 @@ class Viewer:
     def _create_lines02(self, *args):
         self.lines02 = [bq.Lines(x=[], y=[], scales={'x':self.x_scale02, 'y': self.y_scale02}) for i in range(len(self.select_files_lfdl.value))]
         self.fig02.marks = self.lines02
+
+    def _create_lines03(self, *args):
+        with self.out03:
+            self.lines03 = [bq.Lines(x=[], y=[], scales={'x':self.x_scale03, 'y': self.y_scale03},colors=['red']) for i in range(len(self.select01.value))]
+            # self.fig03.marks = self.lines03
+            # self.lines04 = [bq.Lines(x=[], y=[], scales={'x':self.x_scale03, 'y': self.y_scale03}) for i in range(len(self.select02.value))]
+
+            # self.fig03.marks = [self.lines03, self.lines04]
+            # print(self.lines03)
+            # print(self.lines04)
+
+    def _create_lines04(self, *args):
+        with self.out03:
+            self.lines04 = [bq.Lines(x=[], y=[], scales={'x':self.x_scale03, 'y': self.y_scale03},colors=['blue']) for i in range(len(self.select02.value))]
+            # self.lines03.append(np.squeeze(self.lines04))
+            self.lines03 = self.lines03 + self.lines04
+            self.fig03.marks = self.lines03
 
     def button_show_path(self, *args):
         try:
@@ -118,12 +171,21 @@ class Viewer:
         except:
             pass
 
+    def _button_show_all(self, *args):
+        try:
+            folder01 = pathlib.Path(self.path_01.value)
+            files01 = folder01.rglob('*full_output*.csv')
+            self.select01.options = [i for i in files01]
+            self.column_y_01.options = pd.read_csv(self.select01.options[0], skiprows=[0,2], na_values=-9999, parse_dates=[['date','time']]).columns.to_list()
+
+            folder02 = pathlib.Path(self.path_02.value)
+            files02 = folder02.rglob('TOA5*.flux.dat')
+            self.select02.options = [i for i in files02]
+            self.column_y_02.options = pd.read_csv(self.select02.options[0], skiprows=[0,2,3], na_values='NAN', parse_dates=['TIMESTAMP']).columns.to_list()
+        except:
+            pass
 
     def button_plot(self, *args):
-        # self.out.clear_output()
-        # with self.out:
-        #     for i in self.select_files.value:
-        #         print(i)
         with self.out:
 
             dataframes = [pd.read_csv(i, skiprows=[0,2], na_values=-9999, parse_dates=[['date','time']]) for i in self.select_files.value]
@@ -145,3 +207,19 @@ class Viewer:
             for i, f in enumerate(zip(dataframes, self.select_files_lfdl.value)):
                 self.lines02[i].x = f[0]['{}'.format(self.column_x_axis_lfdl.value)].to_list()
                 self.lines02[i].y = f[0]['{}'.format(self.column_y_axis_lfdl.value)].to_list()
+
+    def _button_plot_compare(self, *args):
+        dataframes01 = [pd.read_csv(i, skiprows=[0,2], na_values=-9999, parse_dates=[['date','time']]) for i in self.select01.value]
+        dataframes02 = [pd.read_csv(i, skiprows=[0,2,3], na_values='NAN', parse_dates=['TIMESTAMP']) for i in self.select02.value]
+
+        self.xax03.label = 'Time'
+        self.yax03_01.label = self.column_y_01.value
+        self.yax03_02.label = self.column_y_02.value
+
+        for i,f in enumerate(dataframes01):
+            self.lines03[i].x = f['{}'.format('date_time')].to_list()
+            self.lines03[i].y = f['{}'.format(self.column_y_01.value)].to_list()
+
+        for i, f in enumerate(dataframes02, start=len(dataframes01)):
+            self.lines03[i].x = f['{}'.format('TIMESTAMP')].to_list()
+            self.lines03[i].y = f['{}'.format(self.column_y_02.value)].to_list()
